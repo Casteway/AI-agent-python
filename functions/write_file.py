25fb4ce1555f0b91c3e90100af962b8f4de512e6
Krystian <napier2211@gmail.com>
2025-11-04 15:28:17 +0100

    added writing files

---
+++ functions/write_file.py
@@ -0,0 +1,25 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_work_dir = os.path.abspath(working_directory)
+    abs_full_path = os.path.abspath(os.path.join(abs_work_dir, file_path))
+
+    if not abs_full_path.startswith(abs_work_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.exists(abs_full_path):
+        try:
+            os.makedirs(os.path.dirname(abs_full_path), exist_ok=True)
+        except Exception as e:
+            return f"Error: creating directory: {e}"
+        
+    if os.path.exists(abs_full_path) and os.path.isdir(abs_full_path):
+        return f'Error: "{file_path}" is a directory, not a file'
+    try:
+        with open(abs_full_path, "w") as f:
+            f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+    except Exception as e:
+        return f"Error: writing to file: {e}"
