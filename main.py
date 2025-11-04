1e340fd5a3ef313882751c4f20a2a1252d07cfba
Krystian <napier2211@gmail.com>
2025-11-04 07:53:00 +0100

    checking flags

--- main.py
+++ main.py
@@ -10,8 +10,15 @@ def main():
 
     args = sys.argv[1:]
 
+    is_verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith('--'):
+            args.append(arg)
+
     if not args:
         print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
@@ -25,17 +32,20 @@ def main():
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)      
+    generate_content(client, messages, is_verbose, user_prompt)      
 
-def generate_content(client, messages):
+def generate_content(client, messages, is_verbose, user_prompt):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    if is_verbose:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print(response.text)
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
